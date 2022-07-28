4a38975b4067c8911d122235014e5ef3b55eb9c9
DioguinhoSantos <diiogosantos@live.com>
2022-07-28 19:53:07 -0300

    Barbosa-A-Diogo-Santos

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+let apenasNumeros = [14, 7.5, 1];
+let apenasLetrinhas = ["Santos","pão de queijo","cumbuca"];
+let apenasTudo = [1, "devaneio", 5>2];
+
+console.log(apenasNumeros);
+console.log(apenasLetrinhas);
+console.log(apenasTudo);
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,19 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+let apenasNumeros = [14, 7.5, 1];
+let apenasLetrinhas = ["Santos","pão de queijo","cumbuca"];
+let apenasTudo = [1, "devaneio", 5>2];
+
+console.log(apenasNumeros);
+console.log(apenasLetrinhas);
+console.log(apenasTudo);
+
+
+console.log(apenasNumeros.length);
+console.log(apenasLetrinhas.length);
+console.log(apenasTudo.length);
+
+console.log(apenasNumeros[0]);
+console.log(apenasLetrinhas[1]);
+console.log(apenasNumeros.includes(14));
+console.log(apenasTudo.includes(9));
